d7eeb0e2e4c48b5008de61aa3b6c6a8279b3eb75
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-09 04:26:13 +0430

    Add data insert queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+INSERT INTO animals VALUES (1, 'Agumon','feb/3/2020',0,true,10.23);
+INSERT INTO animals VALUES (2, 'Gabumon','nov/15/2018',2,true,8);
+INSERT INTO animals VALUES (3, 'Pikachu','jan/7/2021',1,false,15.04);
+INSERT INTO animals VALUES (4, 'Devimon','may/12/2017',5,true,11);
