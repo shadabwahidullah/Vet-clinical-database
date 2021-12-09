0e65c97e1d93e72e9d05b6070f7d59f949ba6ad0
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-09 04:56:02 +0430

    Write select queries

--- queries.sql
+++ queries.sql
@@ -1,6 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
-
-INSERT INTO animals VALUES (1, 'Agumon','feb/3/2020',0,true,10.23);
-INSERT INTO animals VALUES (2, 'Gabumon','nov/15/2018',2,true,8);
-INSERT INTO animals VALUES (3, 'Pikachu','jan/7/2021',1,false,15.04);
-INSERT INTO animals VALUES (4, 'Devimon','may/12/2017',5,true,11);
+SELECT * from animals WHERE name LIKE '%mon';
+SELECT name from animals WHERE date_of_birth BETWEEN '1/1/2016' AND '1/1/2019';
+SELECT name from animals WHERE neutured = true AND escape_attempts < 3;
+SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutured = true;
+SELECT * FROM animals WHERE NOT name = 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
