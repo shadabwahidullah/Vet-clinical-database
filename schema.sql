f73a58262abbdab22f48338aa6964e323bde1460
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-09 22:51:23 +0430

    Resolve comments

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
+createdb vet_clinic
 
 CREATE TABLE animals (
-    name varchar(100)
+    id int PRIMARY KEY GENERATED  ALWAYS AS IDENTITY,,
+    name varchar(255),
+    date_of_birth DATE,
+    escape_attempts integer,
+    neutured boolean,
+    weight_kg DECIMAL,
 );
