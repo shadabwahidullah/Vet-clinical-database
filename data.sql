36bebe206af8dc1666576d185898147a2a33d145
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-09 22:50:00 +0430

    Resolve comments

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals VALUES (1, 'Agumon','feb/3/2020',0,true,10.23);
-INSERT INTO animals VALUES (2, 'Gabumon','nov/15/2018',2,true,8);
-INSERT INTO animals VALUES (3, 'Pikachu','jan/7/2021',1,false,15.04);
-INSERT INTO animals VALUES (4, 'Devimon','may/12/2017',5,true,11);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutured, weight_kg) VALUES (1, 'Agumon','feb/3/2020',0,true,10.23);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutured, weight_kg) VALUES (2, 'Gabumon','nov/15/2018',2,true,8);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutured, weight_kg) VALUES (3, 'Pikachu','jan/7/2021',1,false,15.04);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutured, weight_kg) VALUES (4, 'Devimon','may/12/2017',5,true,11);
