34096647d452e7f73c764b3b192622c40f25f3d6
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-10 02:58:54 +0430

    Resolve issues with create queries

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 createdb vet_clinic
 
 CREATE TABLE animals (
-    id int PRIMARY KEY GENERATED  ALWAYS AS IDENTITY,,
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255),
     date_of_birth DATE,
     escape_attempts integer,
@@ -13,8 +13,12 @@ CREATE TABLE animals (
 ALTER TABLE animals ADD COLUMN species varchar(255);
 
 CREATE TABLE owners (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     full_name VARCHAR(255),
     age INTEGER,
 );
 
+CREATE TABLE species (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255)
+);
